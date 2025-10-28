ad9e9920fd2639ae3675de0db22f256c9c03b793
CHEGURI VENKATESHAM <137918601+Venkatesh290@users.noreply.github.com>
2025-10-28 21:37:08 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (7.17%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        from collections import Counter
+        freq = Counter(s)
+        length = 0
+        odd_found = False
+
+        for count in freq.values():
+            length += count // 2 * 2
+            if count % 2 == 1:
+                odd_found = True
+        return length + 1 if odd_found else length
